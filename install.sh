c52e1e0da93a045e307ebd670b61c8cb9c7b2140
Christian Wiedemann <christian.wiedemann@capgemini.com>
2020-11-08 13:12:58 +0100

    add install

---
+++ install.sh
@@ -0,0 +1,6 @@
+# maven
+# https://developers.redhat.com/blog/2020/02/26/speed-up-maven-builds-in-tekton-pipelines/
+#https://github.com/tektoncd/catalog/tree/master/task/maven/0.2
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/maven/0.2/maven.yaml
+
+
